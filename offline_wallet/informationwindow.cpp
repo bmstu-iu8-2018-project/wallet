484d2dcd80527f11a40e5fcfebd3ec32b94a3c0a
belgorodtsev <32188418+belgorodtsev@users.noreply.github.com>
2018-11-15 23:35:14 +0300

    add listview

--- offline_wallet/informationwindow.cpp
+++ offline_wallet/informationwindow.cpp
@@ -10,12 +10,18 @@ InformationWindow::InformationWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
+    fs_model_ = new QFileSystemModel(this);
+    fs_model_->setFilter(QDir::Files | QDir::Dirs);
+
     QString path_private_data = get_path_private_dir();
 
     ui->name->setText(JsonWallet::get_name(path_private_data + "/authorization_data.json"));
     ui->addres->setText(JsonWallet::get_address(path_private_data + "/address_public_key.json"));
     ui->public_key->setText(JsonWallet::get_public_key(path_private_data + "/address_public_key.json"));
 }
+
+QString InformationWindow::mark_path_;
+char InformationWindow::name_device_;
 
 QString InformationWindow::get_path_private_dir()
 {
@@ -33,4 +39,125 @@ QString InformationWindow::get_path_private_dir()
 InformationWindow::~InformationWindow()
 {
     delete ui;
+}
+
+void InformationWindow::finde_usb_device()
+{
+    mon = usb_monitor::create();
+    mon->on_device_add(device_added);
+
+    mon->mount_existing_devices();
+    mon->start();
+}
+
+void InformationWindow::device_added(char letter)
+{
+    name_device_ = letter;
+}
+
+void InformationWindow::chek_device()
+{
+    std::set<wchar_t> set_device = mon->get_flash_disks(1);
+
+    while (set_device.empty())
+    {
+        if (set_device.empty())
+            QMessageBox::warning(this, "Message", "Insert USB device!");
+
+        set_device = mon->get_flash_disks(1);
+    }
+    mon->stop();
+    chec_mark_on_device(get_device_path());
+}
+
+void InformationWindow::chec_mark_on_device(QString path)
+{
+    bool flag = false;
+
+    QDirIterator it(path, QStringList() << "*.dat", QDir::Files, QDirIterator::Subdirectories);
+
+    while (it.hasNext())
+    {
+        it.next();
+
+        if (it.fileName() == "mark.dat")
+        {
+            flag = true;
+            mark_path_ = it.filePath();
+            break;
+        }
+    }
+
+    if (!flag)
+    {
+        QMessageBox::warning(this, "Message", "You inserted an incorrect USB flash drive!\nTry again");
+        chek_device();
+        chec_mark_on_device(get_device_path());
+    }
+
+}
+
+QString InformationWindow::get_name_wallet()
+{
+    QDir dir(mark_path_);
+    dir.cdUp();
+    return dir.dirName();
+}
+
+QString InformationWindow::get_device_path()
+{
+    QString path;
+    path.push_back(name_device_);
+    path += ":/";
+    return path;
+}
+
+QString InformationWindow::get_transactions_path()
+{
+    QString path;
+    QDir dir(mark_path_);
+    dir.cdUp();
+    path = dir.path();
+    path += "/Transactions/";
+    return path;
+}
+
+void InformationWindow::on_sing_transaction_clicked()
+{
+
+}
+
+void InformationWindow::on_update_trans_clicked()
+{
+    finde_usb_device();
+    chek_device();
+    if (get_name_wallet() == ui->name->text())
+    {
+        fs_model_->setRootPath(get_device_path());
+        ui->transactoin_list->setModel(fs_model_);
+    }
+    else
+    {
+        QMessageBox::warning(this, "Message", "Bad");
+    }
+}
+
+void InformationWindow::on_transactoin_list_doubleClicked(const QModelIndex &index)
+{
+    QListView *list_view = (QListView*)sender();
+    QFileInfo file_info = fs_model_->fileInfo(index);
+    if (file_info.fileName() == "..")
+    {
+        QDir dir = file_info.dir();
+        dir.cdUp();
+        list_view->setRootIndex(fs_model_->index(dir.absolutePath()));
+    }
+    else if (file_info.fileName() == ".")
+    {
+        list_view->setRootIndex(fs_model_->index(get_transactions_path()));
+    }
+    else if (file_info.isDir())
+    {
+        list_view->setRootIndex(index);
+    }
 }
--- offline_wallet/informationwindow.h
+++ offline_wallet/informationwindow.h
@@ -2,6 +2,9 @@
 #define INFORMATIONWINDOW_H
 
 #include <QWidget>
+#include <QListView>
+#include <QFileSystemModel>
+#include <includes/usb_monitor.h>
 
 namespace Ui
 {
@@ -16,11 +19,36 @@ class InformationWindow : public QWidget
     explicit InformationWindow(QWidget *parent = nullptr);
     ~InformationWindow();
 
- private:
+private slots:
+    void on_sing_transaction_clicked();
+
+    void on_update_trans_clicked();
+
+    void on_transactoin_list_doubleClicked(const QModelIndex &index);
+
+private:
     Ui::InformationWindow *ui;
+    usb_monitor *mon;
+    QFileSystemModel *fs_model_;
+    static QString mark_path_;
+
+    static char name_device_;
 
     QString get_path_private_dir();
 
+    void finde_usb_device();
+
+    static void device_added(char letter);
+
+    void chek_device();
+
+    void chec_mark_on_device(QString path);
+
+    QString get_device_path();
+
+    QString get_transactions_path();
+
+    static QString get_name_wallet();
 };
 
 #endif // INFORMATIONWINDOW_H
